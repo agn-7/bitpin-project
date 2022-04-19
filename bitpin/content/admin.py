1fdd9e82598aec6c191dd74d520cd932c2d9d982
agn-7 <benyaminjmf@gmail.com>
2022-04-19 16:23:56 +0430

    add conent app including rate and content

--- bitpin/content/admin.py
+++ bitpin/content/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Content, OverallStarRating, StarRating
+
+
+class Rate(admin.TabularInline):
+    model = StarRating
+    extra = 0
+
+
+class ContentAdmin(admin.ModelAdmin):
+    inlines = [Rate, ]
+
+
+# admin.site.register(Content, ContentAdmin)  # TODO :: it didn't work!
+admin.site.register(Content)
+admin.site.register(OverallStarRating)
+admin.site.register(StarRating)
---
+++ bitpin/content/managers.py
@@ -0,0 +1,7 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+
+class OverallStarRatingManager(models.Manager):
+    ...
