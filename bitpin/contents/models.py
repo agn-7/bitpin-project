ffc4cbf74f42b6aebf26b9c04ff135474a892d56
agn-7 <benyaminjmf@gmail.com>
2022-04-20 01:12:40 +0430

    refactoring

--- bitpin/contents/models.py
+++ bitpin/contents/models.py
@@ -4,15 +4,6 @@ from django.conf import settings
 from django.db.models import Avg
 
 
-SCORE_CHOICES = (
-    (1, _("Terrible")),
-    (2, _("Poor")),
-    (3, _("Average")),
-    (4, _("Very Good")),
-    (5, _("Excellent")),
-)
-
-
 class Content(models.Model):
     title = models.CharField(_("Title"), max_length=50)
     text = models.TextField(_("Conetent field"), max_length=500, blank=True)
