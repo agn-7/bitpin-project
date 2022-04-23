09a9ca9c15c4e8f81c5978fdd4a1812b14396d3b
agn-7 <benyaminjmf@gmail.com>
2022-04-23 21:16:08 +0430

    minor

--- bitpin/contents/admin.py
+++ bitpin/contents/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 
+from star_ratings.models import UserRating, Rating
 from .models import Content
 
 
-# class Rate(admin.TabularInline):
+# class RatingAdmin(admin.TabularInline):
+#     def get_queryset(self, request):
+#         return super(RatingAdmin, self).get_queryset(request).select_related('rating', 'user').prefetch_related('rating__content')
 #     model = Rating
 #     extra = 0
 
 
 # class ContentAdmin(admin.ModelAdmin):
-#     inlines = [Rate, ]
+#     inlines = [RatingAdmin, ]
 
 
 admin.site.register(Content)
