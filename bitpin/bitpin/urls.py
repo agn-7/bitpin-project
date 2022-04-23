c727684d4187704732ffa284cee865f7c81226d7
agn-7 <benyaminjmf@gmail.com>
2022-04-24 01:27:27 +0430

    add swagger, auth token, and api/v1

--- bitpin/bitpin/urls.py
+++ bitpin/bitpin/urls.py
@@ -14,8 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.authtoken import views
+
+
+openapi_info = openapi.Info(
+    title="bitpin API",
+    default_version='v1',
+    description="API to access bitpin",
+    terms_of_service="https://github.com/agn-7/bitpin",
+    license=openapi.License(name="MIT License"),
+)
+
+schema_view = get_schema_view(
+    openapi_info,
+    public=True,
+    permission_classes=()
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        route='api/v1/',
+        view=include('contents.urls')
+    ),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=86400), name='api_docs'),
 ]
