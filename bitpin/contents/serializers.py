afa25cb43781eb8f5138f4a0b25bf4546a12762f
agn-7 <benyaminjmf@gmail.com>
2022-04-23 23:24:13 +0430

    add user rate

--- bitpin/contents/serializers.py
+++ bitpin/contents/serializers.py
@@ -1,19 +1,27 @@
 from rest_framework import serializers
 
 from .models import Content
-from star_ratings.models import Rating
-from star_ratings.serializers import RatingSerializer
+from star_ratings.models import Rating, UserRating
+from star_ratings.serializers import RatingSerializer, UserRatingSerializer
 
 
 class ContentSerializer(serializers.ModelSerializer):
     rate = serializers.SerializerMethodField()
+    user_rate = serializers.SerializerMethodField()    
 
+    def get_rate(self, instance):
+        self.rating = Rating.objects.filter(content=instance)
+        serializer = RatingSerializer(instance=self.rating,
+                                      many=True, read_only=True)
+        return serializer.data
+    
+    def get_user_rate(self, instance):
+        user_id = self.context.get("user_id")
+        user_rating = UserRating.objects.filter(rating=self.rating[0], user=user_id)
+        serializer = UserRatingSerializer(instance=user_rating,
+                                          many=True, read_only=True)
+        return serializer.data
+    
     class Meta:
         fields = "__all__"
         model = Content
-
-    def get_rate(self, instance):
-        rating_models = Rating.objects.filter(content=instance)
-        serializer = RatingSerializer(instance=rating_models,
-                                    many=True, read_only=True)
-        return serializer.data
