e7bd2a783ecd5a1397514ad924cfb32936f527e6
agn-7 <benyaminjmf@gmail.com>
2022-04-25 02:09:22 +0430

    change debug, add postgres, static_root

--- bitpin/bitpin/settings.py
+++ bitpin/bitpin/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dk2dku9_=d%h2y7it6_&#_!-ih@j!(zqv0il)p5t!#hn#5k&$*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -84,11 +86,23 @@ WSGI_APPLICATION = 'bitpin.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bitpin_db',
+        'USER': 'bitpin_usr',
+        'PASSWORD': 'root',
+        'HOST': 'bitpin_db',
+        'PORT': '',
+        'TEST': {
+            'NAME': 'bitpin_test_db',
+        }
     }
 }
+
 
 
 # Password validation
@@ -126,6 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
