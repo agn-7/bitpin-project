a419dcebbe1d1c962be73acf6d94c324c311ace8
agn-7 <benyaminjmf@gmail.com>
2022-04-28 01:49:11 +0430

    extend the written models unit test

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,11 +3,21 @@ import pytest
 # from hypothesis.extra.django import TestCase
 
 from contents.models import Content
+from star_ratings.models import UserRating, Rating
+from users.models import CustomUser
 
 
 @pytest.mark.django_db
-def test_create_category():
+def test_create_content():
     content = Content.objects.create(title="Test", text="Something else.")
     assert content.title == "Test"
     assert content.text == "Something else."
     assert Content.objects.filter(title="Test", text="Something else.").exists()
+
+    user = CustomUser.objects.create(username="admin", password="admin")
+    rating = Rating.objects.create(count=0, average=0, content=content)
+    user_rating = UserRating.objects.create(user=user, score=5, rating=rating)
+
+    assert rating.count == 1
+    assert rating.average == 5
+    assert Rating.objects.filter(count=1, average=5, content=content).exists()
