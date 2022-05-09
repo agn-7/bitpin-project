9e1177c43bcf65cf52556ca53b1eaddc59f806f0
agn-7 <benyaminjmf@gmail.com>
2022-05-10 01:54:26 +0430

    add uvicorn and using asgi over wsgi

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ sleep 1
 python manage.py makemigrations &&
 python manage.py migrate --settings=bitpin.product_settings
 
-gunicorn bitpin.wsgi:application --bind 0.0.0.0:8000 -w 2
+gunicorn bitpin.asgi:application --bind 0.0.0.0:8000 -w 2 -k uvicorn.workers.UvicornWorker
