af22711a882219eca701319bb806e6ef98016052
agn-7 <benyaminjmf@gmail.com>
2022-05-12 02:09:12 +0430

    ignore static and create it in container

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 sleep 1
 
-python manage.py makemigrations &&
+python manage.py makemigrations --settings=bitpin.product_settings &&
 python manage.py migrate --settings=bitpin.product_settings
+python manage.py collectstatic --no-input
 
 gunicorn bitpin.wsgi:application --bind 0.0.0.0:8000 -w 2
