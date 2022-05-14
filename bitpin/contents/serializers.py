f563e5d606171c6823099e666c6842f617569966
agn-7 <benyaminjmf@gmail.com>
2022-05-14 12:39:37 +0430

    remove a redundant serilizerMethod

--- bitpin/contents/serializers.py
+++ bitpin/contents/serializers.py
@@ -6,17 +6,12 @@ from star_ratings.serializers import RatingSerializer, UserRatingSerializer
 
 
 class ContentSerializer(serializers.ModelSerializer):
-    rate = serializers.SerializerMethodField()
     user_rate = serializers.SerializerMethodField()
-
-    def get_rate(self, instance):
-        self.rating = Rating.objects.filter(content=instance)
-        serializer = RatingSerializer(instance=self.rating, many=True, read_only=True)
-        return serializer.data
+    rates = RatingSerializer(source="rating")
 
     def get_user_rate(self, instance):
         user_id = self.context.get("user_id")
-        user_rating = UserRating.objects.filter(rating=self.rating[0], user=user_id)
+        user_rating = instance.rating.user_ratings.filter(user=user_id)
         serializer = UserRatingSerializer(
             instance=user_rating, many=True, read_only=True
         )
